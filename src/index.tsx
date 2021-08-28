c54c11d112c49c1d02168ffb638416fdcde0ac69
Alan <alanleung1909@gmail.com>
2021-08-28 17:15:59 +0800

    initial color mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,22 @@ import './i18n';
 import * as React from 'react';
 import { App } from './App';
 import ReactDOM from 'react-dom';
-import { ColorModeScript, theme } from '@chakra-ui/react';
+import { ColorModeScript, extendTheme, ThemeConfig } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
 
+const config: ThemeConfig = {
+	initialColorMode: 'dark',
+	useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 ReactDOM.render(
 	<React.StrictMode>
-		<ColorModeScript initialColorMode="dark" />
 		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<HelmetProvider>
 				<Router>
 					<App />
